61caba8efa6f05fc50b3326c50b86b48b81d8916
Sandeep Pagade <sandyyy2411@gmail.com>
2024-11-27 15:32:57 +0530

    replace CRA with VITE

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import "./App.css";
+import { useState } from "react";
 import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
 import Header from "./components/header/Header";
 import List from "./components/list/List";
 import Map from "./components/map/Map";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <CssBaseline />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Include all React files
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust if you have different file types
   ],
   theme: {
-    extend: {}, // Customize your theme here
+    extend: {},
   },
   plugins: [],
 };
